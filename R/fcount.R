992b521a708161f2684d3dece24a8ddfaecbc39c
Scott Henderson <scottyhq@gmail.com>
2020-08-05 12:43:04 -0700

    Update fcount.R

--- R/fcount.R
+++ R/fcount.R
@@ -3,12 +3,9 @@
 #' @param x factor
 #'
 #' @return A tibble
-#' @import stringi
 #' @export
 #' @examples
 #' fcount(iris$Species)
 fcount <- function(x) {
   forcats::fct_count(x, sort = TRUE)
 }
-
-stringi::stri_length(c('abc', '123', '\u0105\u0104'))
